e604bb9e7716e0ffb2505746b6af0f739ebee952
dusan003 <dusan003gordanic@gmail.com>
2024-12-03 15:33:00 +0100

    Dodavanje pregleda postova u ProfilePage.tsx fajlu
    Pregled svih svojih postova

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,182 +1,212 @@
 import { useState, useEffect } from "react";
 import { UserRegister } from "../models/userModel";
-import { getUserProfile } from "../services/authService";
-import { updateUserProfile } from "../services/authService";  // Pretpostavljam da postoji funkcija za ažuriranje profila
+import { getUserProfile, updateUserProfile } from "../services/authService";
 import { jwtDecode } from "jwt-decode";
 import { Navbar } from "../components/Navbar";
-import { CustomJwtPayload } from "../components/ProtectRoutes";
-import "../assets/ProfilePage.css"
-import React from 'react';
+import { CustomJwtPayload, DecodeToken } from "../components/ProtectRoutes";
+import "../assets/ProfilePage.css";
+import React from "react";
+import { GetUserPosts } from "../services/postService";
+import { Post } from "../models/postModel";
+import { PostCard } from "../components/PostCard";
 
 export const ProfilePage = () => {
-    const [userProfile, setUserProfile] = useState<UserRegister | null>(null);
-    const [userId,setUserId] = useState('');
-    const [editing, setEditing] = useState<boolean>(false);
-    const [updatedProfile, setUpdatedProfile] = useState<UserRegister | null>(null);
-    const [password, setPassword] = useState('');
-    const [showPassword, setShowPassword] = useState(false);
+  const [userProfile, setUserProfile] = useState<UserRegister | null>(null);
+  const [userId, setUserId] = useState("");
+  const [editing, setEditing] = useState<boolean>(false);
+  const [updatedProfile, setUpdatedProfile] = useState<UserRegister | null>(null);
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [userPosts, setUserPosts] = useState<Post[]>([]);
+  const [loadingPosts, setLoadingPosts] = useState<boolean>(true);
 
-    useEffect(() => {
-        const token = localStorage.getItem('token'); 
-        let user_id =null;
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    let user_id = "";
 
-        if (token) {
-            const decodedToken = jwtDecode<CustomJwtPayload>(token); 
-            setUserId(decodedToken.sub); 
-            user_id= decodedToken.sub;
-        }
-        if (user_id) {
-            fetchUserProfile(user_id);
-        }
-    }, []);
+    if (token) {
+      const decodedToken = jwtDecode<CustomJwtPayload>(token);
+      setUserId(decodedToken.sub);
+      user_id = decodedToken.sub;
+    }
 
-    const fetchUserProfile = async (user_id: string) => {
-        try {
-            const profile = await getUserProfile(user_id);
-            setUserProfile(profile);
-            setUpdatedProfile(profile);
-        } catch (error) {
-            console.error('Error fetching user profile:', error);
-            alert("Failed to load profile");
-        }
-    };
+    if (user_id !== "") {
+      fetchUserProfile(user_id);
+      fetchUserPosts(user_id);
+    }
+  }, []);
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const { name, value } = e.target;
-        if (updatedProfile) {
-          setUpdatedProfile({ ...updatedProfile, [name]: value });
-        }
-    };
+  const fetchUserProfile = async (user_id: string) => {
+    try {
+      const profile = await getUserProfile(user_id);
+      setUserProfile(profile);
+      setUpdatedProfile(profile);
+    } catch (error) {
+      console.error("Error fetching user profile:", error);
+      alert("Failed to load profile");
+    }
+  };
 
-    const handleSave = async () => {
-        if (updatedProfile) {
-            try {
-                await updateUserProfile(userId,updatedProfile); 
-                setUserProfile(updatedProfile);
-                alert("Profile updated successfully");
-            } catch (error) {
-                console.error('Error updating profile:', error);
-                alert("Failed to update profile");
-            }
-        }
-    };
+  const fetchUserPosts = async (user_id: string) => {
+    try {
+      const posts = await GetUserPosts(user_id);
+      setUserPosts(posts);
+      setLoadingPosts(false);
+    } catch (error) {
+      console.error("Error fetching user posts:", error);
+      setLoadingPosts(false);
+    }
+  };
 
-    return (
-        <>
-          <Navbar />
-          <div className="profile-container">
-            {userProfile ? (
-              <div className="profile-details">
-                <h1>Profile of {userProfile.username}</h1>
-                <div className="form-group">
-                  <label>Username</label>
-                  <input
-                    type="text"
-                    name="username"
-                    value={updatedProfile?.username}
-                    onChange={handleChange}
-                    className="form-input"
-                  />
-                </div>
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    if (updatedProfile) {
+      setUpdatedProfile({ ...updatedProfile, [name]: value });
+    }
+  };
 
-                <div className="form-group">
-                  <label>Password</label>
-                  <div className="password-input">
-                      <input
-                          type={showPassword ? "text" : "password"}
-                          name="password"
-                          onChange={handleChange}
-                          placeholder="New Password"
-                          className="form-input"
-                      />
-                  </div>
+  const handleSave = async () => {
+    if (updatedProfile) {
+      try {
+        await updateUserProfile(userId, updatedProfile);
+        setUserProfile(updatedProfile);
+        alert("Profile updated successfully");
+      } catch (error) {
+        console.error("Error updating profile:", error);
+        alert("Failed to update profile");
+      }
+    }
+  };
+
+  return (
+    <>
+      <Navbar />
+      <div className="profile-container">
+        {userProfile ? (
+          <>
+            <div className="profile-details">
+              <h1>Profile of {userProfile.username}</h1>
+              <div className="form-group">
+                <label>Username</label>
+                <input
+                  type="text"
+                  name="username"
+                  value={updatedProfile?.username}
+                  onChange={handleChange}
+                  className="form-input"
+                />
               </div>
-      
-                <div className="form-group">
-                  <label>Name</label>
-                  <div className="name-inputs">
-                    <input
-                      type="text"
-                      name="first_name"
-                      value={updatedProfile?.first_name}
-                      onChange={handleChange}
-                      placeholder="First Name"
-                      className="form-input"
-                    />
-                    <input
-                      type="text"
-                      name="last_name"
-                      value={updatedProfile?.last_name}
-                      onChange={handleChange}
-                      placeholder="Last Name"
-                      className="form-input"
-                    />
-                  </div>
-                </div>
-      
-                <div className="form-group">
-                  <label>Email</label>
+
+              <div className="form-group">
+                <label>Password</label>
+                <div className="password-input">
                   <input
-                    type="email"
-                    name="email"
-                    value={updatedProfile?.email}
+                    type={showPassword ? "text" : "password"}
+                    name="password"
                     onChange={handleChange}
+                    placeholder="New Password"
                     className="form-input"
                   />
                 </div>
-      
-                <div className="form-group">
-                  <label>Mobile</label>
-                  <input
-                    type="text"
-                    name="mobile"
-                    value={updatedProfile?.mobile}
-                    onChange={handleChange}
-                    className="form-input"
-                  />
-                </div>
-      
-                <div className="form-group">
-                  <label>Address</label>
-                  <input
-                    type="text"
-                    name="address"
-                    value={updatedProfile?.address}
-                    onChange={handleChange}
-                    className="form-input"
-                  />
-                </div>
-      
-                <div className="form-group">
-                  <label>City</label>
-                  <input
-                    type="text"
-                    name="city"
-                    value={updatedProfile?.city}
-                    onChange={handleChange}
-                    className="form-input"
-                  />
-                </div>
-      
-                <div className="form-group">
-                  <label>Country</label>
-                  <input
-                    type="text"
-                    name="country"
-                    value={updatedProfile?.country}
-                    onChange={handleChange}
-                    className="form-input"
-                  />
-                </div>
-      
-                <button className="save-button" onClick={handleSave}>
-                  Save Changes
-                </button>
               </div>
-            ) : (
-              <p>Loading user profile...</p>
-            )}
-          </div>
-        </>
-    );      
+
+              <div className="form-group">
+                <label>Name</label>
+                <div className="name-inputs">
+                  <input
+                    type="text"
+                    name="first_name"
+                    value={updatedProfile?.first_name}
+                    onChange={handleChange}
+                    placeholder="First Name"
+                    className="form-input"
+                  />
+                  <input
+                    type="text"
+                    name="last_name"
+                    value={updatedProfile?.last_name}
+                    onChange={handleChange}
+                    placeholder="Last Name"
+                    className="form-input"
+                  />
+                </div>
+              </div>
+
+              <div className="form-group">
+                <label>Email</label>
+                <input
+                  type="email"
+                  name="email"
+                  value={updatedProfile?.email}
+                  onChange={handleChange}
+                  className="form-input"
+                />
+              </div>
+
+              <div className="form-group">
+                <label>Mobile</label>
+                <input
+                  type="text"
+                  name="mobile"
+                  value={updatedProfile?.mobile}
+                  onChange={handleChange}
+                  className="form-input"
+                />
+              </div>
+
+              <div className="form-group">
+                <label>Address</label>
+                <input
+                  type="text"
+                  name="address"
+                  value={updatedProfile?.address}
+                  onChange={handleChange}
+                  className="form-input"
+                />
+              </div>
+
+              <div className="form-group">
+                <label>City</label>
+                <input
+                  type="text"
+                  name="city"
+                  value={updatedProfile?.city}
+                  onChange={handleChange}
+                  className="form-input"
+                />
+              </div>
+
+              <div className="form-group">
+                <label>Country</label>
+                <input
+                  type="text"
+                  name="country"
+                  value={updatedProfile?.country}
+                  onChange={handleChange}
+                  className="form-input"
+                />
+              </div>
+
+              <button className="save-button" onClick={handleSave}>
+                Save Changes
+              </button>
+            </div>
+
+            <div className="user-posts">
+              <h2>User Posts</h2>
+              {loadingPosts ? (
+                <p>Loading posts...</p>
+              ) : userPosts.length > 0 ? (
+                userPosts.map((post) => <PostCard key={post._id} post={post} />)
+              ) : (
+                <p>No posts available.</p>
+              )}
+            </div>
+          </>
+        ) : (
+          <p>Loading user profile...</p>
+        )}
+      </div>
+    </>
+  );
 };
